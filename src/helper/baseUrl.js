3ea8821cd23815532973177efd3f0e9fb77351f2
md ashif reza <76996887+md-786910@users.noreply.github.com>
2023-03-09 13:17:48 +0530

    chanesg dev url

--- src/helper/baseUrl.js
+++ src/helper/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = process.env.NODE_ENV === 'development' ? "http://localhost:3000" : ""
+export const baseUrl = process.env.NODE_ENV === 'development' ? "http://localhost:3000" : "https://json-server-cjio.onrender.com"
